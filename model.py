f66361d960dfda0d2ac1f7f8e218d532807dfdc4
capDoYeonLee <ghtyu21@naver.com>
2022-03-19 23:25:56 +0900

    ing

--- model.py
+++ model.py
@@ -1,7 +1,66 @@
 from ast import Pass
 import torch
+import torch.nn as nn
 import torchvision
 import numpy 
 
-def ResNet():
-    pass
+# function으로 만들지 말고 class로 다시 구축해보자 
+
+def conv_block_1(in_dim, out_dim, act_fn, stride = 1):
+    """  bottleneck 구조를 만들기 위한 1x1 convolution """
+    model = nn.Sequential(
+        nn.Conv2d(in_dim, out_dim, kernel_size = 1, stride = stride),
+        act_fn
+    )
+    return model
+
+
+
+def conv_block_3(in_dim, out_dim, act_fn):
+    """  bottleneck 구조를 만들기 위한 3x3 convolution """
+    model = nn.Sequential(
+        nn.Conv2d(in_dim, out_dim, kernel_size = 3, stride = 1, padding = 1),
+        act_fn
+    )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+    
+    
+    
